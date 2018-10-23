4092b9269c0e58ef6e9bb057129838c6805f3929
dev-billy <44404782+dev-billy@users.noreply.github.com>
2018-10-23 17:12:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,143 @@
+//startCursive
+
+document.getElementById("oneButton").onclick = function() {
+   
+    var input = "";
+    
+    input = document.getElementById("cursive").value;
+    
+    document.getElementById("cursiveText").innerHTML = input;
+};
+
+//endCursive
+
+//startRoboto
+
+document.getElementById("twoButton").onclick = function() {
+   
+    var input = "";
+    
+    input = document.getElementById("roboto").value;
+    
+    document.getElementById("robotoText").innerHTML = input;
+};
+
+//endRoboto
+
+//startMali
+
+document.getElementById("threeButton").onclick = function() {
+   
+    var input = "";
+    
+    input = document.getElementById("mali").value;
+    
+    document.getElementById("maliText").innerHTML = input;
+};
+
+//endMali
+
+//startChakra
+
+document.getElementById("fourButton").onclick = function() {
+   
+    var input = "";
+    
+    input = document.getElementById("chakra").value;
+    
+    document.getElementById("chakraText").innerHTML = input;
+};
+
+//endChakra
+
+//startOpen
+
+document.getElementById("fiveButton").onclick = function() {
+   
+    var input = "";
+    
+    input = document.getElementById("open").value;
+    
+    document.getElementById("openText").innerHTML = input;
+};
+
+//endChakra
+
+
+//startUbuntu
+
+document.getElementById("sixButton").onclick = function() {
+   
+    var input = "";
+    
+    input = document.getElementById("ubuntu").value;
+    
+    document.getElementById("ubuntuText").innerHTML = input;
+};
+
+//endUbuntu
+
+//startCharm
+
+document.getElementById("sevenButton").onclick = function() {
+   
+    var input = "";
+    
+    input = document.getElementById("charm").value;
+    
+    document.getElementById("charmText").innerHTML = input;
+};
+
+//endCharm
+
+//startindie
+
+document.getElementById("eightButton").onclick = function() {
+   
+    var input = "";
+    
+    input = document.getElementById("indie").value;
+    
+    document.getElementById("indieText").innerHTML = input;
+};
+
+//endindie
+
+//startJosefin
+
+document.getElementById("nineButton").onclick = function() {
+   
+    var input = "";
+    
+    input = document.getElementById("josefin").value;
+    
+    document.getElementById("josefinText").innerHTML = input;
+};
+
+//endJosefin
+
+//startLobster
+
+document.getElementById("tenButton").onclick = function() {
+   
+    var input = "";
+    
+    input = document.getElementById("lobster").value;
+    
+    document.getElementById("lobsterText").innerHTML = input;
+};
+
+//endlobster
+
+//startpacifio
+
+document.getElementById("elevenButton").onclick = function() {
+   
+    var input = "";
+    
+    input = document.getElementById("pacifio").value;
+    
+    document.getElementById("pacifioText").innerHTML = input;
+};
+
+//endpacifio
